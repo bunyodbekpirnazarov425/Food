9bcf75aa976afe0da86035fbb0ff868d248a45d8
Bunyodbek <bunyodbekpirnazarov425@gmail.com>
2024-10-24 16:54:27 +0500

    food

--- app/urls.py
+++ app/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path, include
-from .views import FoodTypeListCreateView, FoodListCreateView, CommentListCreateView
+from .views import FoodTypeListCreateView, FoodListCreateView, CommentListCreateView, FoodRetrieveUpdateDestroyView
 
 urlpatterns = [
-    path('food-types/', FoodTypeListCreateView.as_view(), name='food-type-list-create'),
+    path('food-types/', FoodTypeListCreateView.as_view(), name='foodtype-list-create'),
     path('foods/', FoodListCreateView.as_view(), name='food-list-create'),
+    path('foods/<int:pk>/', FoodRetrieveUpdateDestroyView.as_view(), name='food-detail'),
     path('comments/', CommentListCreateView.as_view(), name='comment-list-create'),
 ]
--- app/views.py
+++ app/views.py
@@ -1,14 +1,19 @@
-from rest_framework.generics import GenericAPIView
+from rest_framework.generics import GenericAPIView, RetrieveUpdateDestroyAPIView
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
 from rest_framework import status, mixins
 from .models import FoodType, Food, Comment
+from .permissions import IsAuthorOrReadOnly
 from .serializers import FoodTypeSerializer, FoodSerializer, CommentSerializer
+from rest_framework.filters import SearchFilter, OrderingFilter
 
 class FoodTypeListCreateView(GenericAPIView):
     queryset = FoodType.objects.all()
     serializer_class = FoodTypeSerializer
     permission_classes = [IsAuthenticated]
+    filter_backends = [SearchFilter, OrderingFilter]
+    search_fields = ['name']
+    ordering_fields = ['name']
 
     def get(self, request, *args, **kwargs):
         food_types = self.get_queryset()
@@ -26,6 +31,9 @@ class FoodListCreateView(GenericAPIView):
     queryset = Food.objects.all()
     serializer_class = FoodSerializer
     permission_classes = [IsAuthenticated]
+    filter_backends = [SearchFilter, OrderingFilter]
+    search_fields = ['name', 'composition']
+    ordering_fields = ['price']
 
     def get(self, request, *args, **kwargs):
         foods = self.get_queryset()
@@ -44,6 +52,9 @@ class CommentListCreateView(mixins.ListModelMixin, mixins.CreateModelMixin, Gene
     queryset = Comment.objects.all()
     serializer_class = CommentSerializer
     permission_classes = [IsAuthenticated]
+    filter_backends = [SearchFilter, OrderingFilter]
+    search_fields = ['text']
+    ordering_fields = ['created']
 
     def get(self, request, *args, **kwargs):
         return self.list(request, *args, **kwargs)
@@ -52,3 +63,7 @@ class CommentListCreateView(mixins.ListModelMixin, mixins.CreateModelMixin, Gene
         return self.create(request, *args, **kwargs)
 
 
+class FoodRetrieveUpdateDestroyView(RetrieveUpdateDestroyAPIView):
+    queryset = Food.objects.all()
+    serializer_class = FoodSerializer
+    permission_classes = [IsAuthenticated, IsAuthorOrReadOnly]
